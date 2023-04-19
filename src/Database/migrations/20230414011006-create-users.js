73d973c36c1e428aedc81f2c4f103594a3450720
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-19 16:28:36 -0300

    feat: model, config, seeder, migration add exemplo

--- src/Database/migrations/20230414011006-create-users.js
+++ src/Database/migrations/20230414011006-create-users.js
@@ -1,10 +1,31 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
   },
-
-  async down (queryInterface, Sequelize) {
-  }
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
 };
---
+++ src/Database/models/UserModel.ts
@@ -0,0 +1,43 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default User;
